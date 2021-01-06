25c51f4efc759a2e50cca868a27516bf03596986
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-01-06 15:59:01 -0300

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+
+
+function App() {
+  return (
+    <div>
+     <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
