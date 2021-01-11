4cc7284cce059311f23484c2e5cb6b95ad0f2d7a
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-01-11 20:38:59 -0300

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/MainComponent';
-
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 
 
 class App extends Component{
   
   render(){
     return(
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
     
   }
